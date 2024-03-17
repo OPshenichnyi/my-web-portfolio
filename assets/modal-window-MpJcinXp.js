543f8b746775af972bb7762a87df124a3657987a
OPshenichnyi <106489713+OPshenichnyi@users.noreply.github.com>
2024-03-17 05:56:39 +0000

    Deploying to gh-pages from @ OPshenichnyi/my-web-portfolio@c782c19837ca95f3a1fd5b7870180fe5a2f682c0 🚀

--- assets/modal-window-MpJcinXp.js
+++ assets/modal-window-MpJcinXp.js
@@ -39,3 +39,14 @@
     fetch(link.href, fetchOpts);
   }
 })();
+const refs = {
+  burgerMenu: document.querySelector(".header__burger-menu"),
+  modalWindow: document.querySelector(".modal_window_backdrop")
+};
+refs.burgerMenu.addEventListener("click", () => {
+  console.log("hi");
+  refs.modalWindow.classList.add("show");
+});
+refs.modalWindow.addEventListener("click", () => {
+  refs.modalWindow.classList.remove("show");
+});
