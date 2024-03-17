794983d3bdf1781013808534fcd8a2c71c284430
OPshenichnyi <106489713+OPshenichnyi@users.noreply.github.com>
2024-03-17 07:38:54 +0000

    Deploying to gh-pages from @ OPshenichnyi/my-web-portfolio@0cd87021c74a68def3663771ac2b6253c37da704 🚀

--- assets/modal-window-Haa7KTC3.js
+++ assets/modal-window-Haa7KTC3.js
@@ -42,11 +42,16 @@
 const refs = {
   burgerMenu: document.querySelector(".header__burger-menu"),
   modalWindow: document.querySelector(".modal_window_backdrop"),
-  close: document.querySelector(".backdrop__close--btn")
+  close: document.querySelector(".backdrop__close--btn"),
+  link: document.querySelector(".modal_link")
 };
 refs.burgerMenu.addEventListener("click", () => {
   refs.modalWindow.classList.add("show");
 });
 refs.close.addEventListener("click", () => {
+  refs.modalWindow.classList.remove("show");
+});
+refs.link.addEventListener("click", () => {
+  console.log("link");
   refs.modalWindow.classList.remove("show");
 });
