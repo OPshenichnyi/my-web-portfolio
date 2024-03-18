3a1406564d9cbc396fc16fd9fab16136b239acc7
OPshenichnyi <106489713+OPshenichnyi@users.noreply.github.com>
2024-03-18 12:01:38 +0200

    add function close modal window for link navigation

--- src/js/modal-window.js
+++ src/js/modal-window.js
@@ -3,10 +3,14 @@
     openModalBtn: document.querySelector('[data-mobile-open]'),
     closeModalBtn: document.querySelector('[data-mobile-close]'),
     modal: document.querySelector('[data-mobile]'),
+    linkCloseModal: document.querySelectorAll('[data-link]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.linkCloseModal.forEach(element => {
+    element.addEventListener('click', toggleModal);
+  });
 
   function toggleModal() {
     refs.modal.classList.toggle('mobile-hidden');
