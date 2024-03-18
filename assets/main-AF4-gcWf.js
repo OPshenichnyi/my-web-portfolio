28d23395feb92ac6afff118e6edb93a1a37201a7
OPshenichnyi <106489713+OPshenichnyi@users.noreply.github.com>
2024-03-18 10:02:00 +0000

    Deploying to gh-pages from @ OPshenichnyi/my-web-portfolio@3a1406564d9cbc396fc16fd9fab16136b239acc7 🚀

--- assets/main-AF4-gcWf.js
+++ assets/main-AF4-gcWf.js
@@ -43,10 +43,14 @@
   const refs = {
     openModalBtn: document.querySelector("[data-mobile-open]"),
     closeModalBtn: document.querySelector("[data-mobile-close]"),
-    modal: document.querySelector("[data-mobile]")
+    modal: document.querySelector("[data-mobile]"),
+    linkCloseModal: document.querySelectorAll("[data-link]")
   };
   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.linkCloseModal.forEach((element) => {
+    element.addEventListener("click", toggleModal);
+  });
   function toggleModal() {
     refs.modal.classList.toggle("mobile-hidden");
   }
