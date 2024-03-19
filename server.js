ba80823d308c2b6bd08256d7eeb7de1fba7a3bb9
OPshenichnyi <106489713+OPshenichnyi@users.noreply.github.com>
2024-03-19 19:56:56 +0200

    add server js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-import { express } from 'express';
-import { path } from 'path';
+import express from 'express';
+import path from 'path';
 const app = express();
 
 const PORT = process.env.PORT || 3000;
