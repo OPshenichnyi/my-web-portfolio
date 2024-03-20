0120cc2e5ae42e476375845fa67870fa9ac5221a
OPshenichnyi <106489713+OPshenichnyi@users.noreply.github.com>
2024-03-20 07:34:17 +0200

    add server js

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,8 @@ import glob from 'fast-glob';
 import { fileURLToPath } from 'url';
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 import IconSpritePlugin from './plugins/vite-plugin-icon-sprite';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   // base: '/my-web-portfolio/',
